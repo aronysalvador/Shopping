058ee09a5637a627daa4d039e51fd0059bc5ac0e
Arony Salvador <arony.salvador@gfusiona.cl>
2019-08-07 18:15:42 -0700

    Desarrollo Carrito
    Incluye el flujo completo de los 3 primos modulos (ListProducts, DetailsProducts, orderCart)

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,88 @@ var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find( function(err,docs){
-
     var productChunks = [];
     var chunkSize = 3 ;
     for(var i = 0; i < docs.length; i += chunkSize){
         productChunks.push(docs.slice(i, i + chunkSize));
-        res.render('index', { products: docs });
-    }
-    
+        res.render('indexProducts', { products: docs });
+    }    
   });
 });
+
+router.get('/details', function(req, res, next) {
+    var id = req.query.id;
+    Product.find( function(err,docs){ 
+      switch (id) {
+        case 'Microscopio':
+          res.render('indexDetails', { products: docs[0]});
+          break;
+        case 'Televisor':
+          res.render('indexDetails', { products: docs[1]});
+          break;
+        case 'Telefono':
+          res.render('indexDetails', { products: docs[2]});
+          break;
+        case 'Lavadora':
+          res.render('indexDetails', { products: docs[3]});
+          break;
+        default:
+          console.log('Lo lamentamos, por el momento no disponemos de Productos' );
+      }
+    });
+});
+
+router.get('/orderCart', function(req, res, next) {
+  var id = req.query.id;
+  Product.find( function(err,docs){ 
+    switch (id) {
+      case 'Microscopio':
+        res.render('indexOrderCarts', { products: docs[0]});
+        break;
+      case 'Televisor':
+        res.render('indexOrderCarts', { products: docs[1]});
+        break;
+      case 'Telefono':
+        res.render('indexOrderCarts', { products: docs[2]});
+        break;
+      case 'Lavadora':
+        res.render('indexOrderCarts', { products: docs[3]});
+        break;
+      default:
+        console.log('Lo lamentamos, por el momento no disponemos de Productos' );
+    }
+  });  
+});
+
+// router.get('/orderCart', function(req, res, next) {
+//   var id = req.query.id;
+//   var getProduct2 = getProduct(id);
+//   Product.find( function(err,docs){     
+//     res.render('indexOrderCarts', { products: docs[getProduct2]});
+//   });  
+// });
+
+// function getProduct(res){
+//   let resGetProduct = "";
+//   Product.find( function(err,docs){ 
+//     switch (res) {
+//       case 'Microscopio':
+//         resGetProduct = 0;
+//         break;
+//       case 'Televisor':
+//         resGetProduct = 1;
+//         break;
+//       case 'Telefono':
+//         resGetProduct = 2;
+//         break;
+//       case 'Lavadora':
+//           resGetProduct = 3;
+//         break;
+//       default:
+//         console.log('Lo lamentamos, por el momento no disponemos de Productos' );
+//     }
+//   });
+//   return resGetProduct
+// }
 
 module.exports = router;
